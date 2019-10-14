49d61def95013567f47ff5932c8b6ac8e08df246
Tom Pocius <poci14@gmail.com>
2019-10-14 13:01:25 -0500

    created rough outline in html with some style in css

---
+++ app.js
@@ -0,0 +1,11 @@
+//set variables
+
+//helper functions
+
+//events
+
+
+//inits: 
+//check local storage object for matching id
+//grab current date and put on html
+//color code past/present and future
