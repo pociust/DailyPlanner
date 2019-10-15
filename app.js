971560bfcb11321467725d0e29858d6bdf1ec1d7
Tom Pocius <poci14@gmail.com>
2019-10-14 21:30:13 -0500

    added js for timer, currently in 24 hour format, has it checking time to change some css, need to have it change on fly

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 //set variables
+dateDisplay = document.getElementById('current-date');
+timeDisplay = document.getElementById('current-time');
 
-//helper functions
 
+function currentDate() {
+  let now = new Date();
+  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  time = `${now.getHours()}:${now.getMinutes()}`,
+  date = [now.getDate(),
+          months[now.getMonth()],
+          now.getFullYear()].join(' ');
+  dateDisplay.textContent = `Today is: ${date}`;
+  timeDisplay.textContent = `It is: ${time}`;
+
+  if (now.getHours() >= 21) {
+    document.getElementById('nine-hour').style.backgroundColor = "red";
+}
+
+}
 //events
-
-
+setInterval(currentDate, 1000);
+currentDate();
 //inits: 
 //check local storage object for matching id
 //grab current date and put on html
