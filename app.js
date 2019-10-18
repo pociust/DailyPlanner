7a9e6ac53b7105d7b911d00062c5cf174ca98fe4
Tom Pocius <poci14@gmail.com>
2019-10-18 00:21:21 -0500

    changed id tags to create more slimline js, might need to rename them again, created new style for text, changed css attrb for hours, added an alert function to remind when notes are about due

--- app.js
+++ app.js
@@ -11,71 +11,97 @@ function checkTime() {
     let currentDate = moment().format('MMM Do YYYY');
     $('#current-date').text(`Today is: ${currentDate}`);
 
-    now = moment().format('H');
+    now = 13;
     let inputTime = $('input');
     idTime = '';
     
     for (i=0; i < inputTime.length; i++) {
       let idTime = inputTime[i].id 
-      idTime = parseInt(idTime.slice(0, -3));
-    
-    
+      idTime = parseInt(idTime);
       if (now > idTime) {
-        $(`#${inputTime[i].id}`).attr('style', 'background-color:blue');
-        // console.log('now', now);
+        $(`#${inputTime[i].id}`).attr('style', 'background-color:#fc0341');
       } else if (now < idTime) {
-        $(`#${inputTime[i].id}`).attr('style', 'background-color:red');
+        $(`#${inputTime[i].id}`).attr('style', 'background-color:#03f4fc');
       } else if (now === idTime) {
-        $(`#${inputTime[i].id}`).attr('style', 'background-color:green');
+        $(`#${inputTime[i].id}`).attr('style', 'background-color:#03fc77');
       }
     };
   }
+};
+
+function alertMessage () {
   
+  
+  let setAlert = '50 0';
+  let nextReminderTime =   moment().add(1,'h').format('h');
+  let nextReminderId = $(`#${nextReminderTime}`);
+  if ((moment().format('h') > 9 || moment().format('h') < 18) && (moment().format('m s') === setAlert)) {
+      alert(`10 min reminder to ${nextReminderId[0].value}`);
+  }
 
-
-}
-
-
+};
+  
 
 $('.save-buttons').click(function () {
   const key = $(this).data('key');
-  let inputText = $('#' + key);
+  let inputText = $(`#${key}`);
   localStorage.setItem(key, inputText[0].value);  
 });
 
+// //need to change classes to 24 hour style and get rid of am/pm 
+
+// console.log(localStorage);
+
+// function getNotes () {
+  
+//   // let enterNotes = JSON.stringify(localStorage);
+
+//   // for(i = 9; i < 18; i++){
+//   //   console.log(localStorage.getItem(Storage.key(i)));
+
+//   // }
+//   // for(i=13; i < 18; i++){
+//   //   if (localStorage.getItem(`$[i]-pm`)) {
+//   //     $(`.js-${i}-am`).val(`${localStorage.getItem(`[i]-pm`)}`);
+//   //   }
+//   // }
+// };
+// getNotes();
 
 
-if (localStorage.getItem('9-am')) {
-  $('.js-9-am').val(`${localStorage.getItem('9-am')}`);
-}
-if (localStorage.getItem('10-am')) {
-  $('.js-10-am').val(`${localStorage.getItem('10-am')}`);
-}
-if (localStorage.getItem('11-am')) {
-  $('.js-11-am').val(`${localStorage.getItem('11-am')}`);
-}
-if (localStorage.getItem('12-am')) {
-  $('.js-12-am').val(`${localStorage.getItem('12-am')}`);
-}
-if (localStorage.getItem('13-pm')) {
-  $('.js-1-pm').val(`${localStorage.getItem('13-pm')}`);
-}
-if (localStorage.getItem('14-pm')) {
-  $('.js-2-pm').val(`${localStorage.getItem('14-pm')}`);
-}
-if (localStorage.getItem('15-pm')) {
-  $('.js-3-pm').val(`${localStorage.getItem('15-pm')}`);
-}
-if (localStorage.getItem('16-pm')) {
-  $('.js-4-pm').val(`${localStorage.getItem('16-pm')}`);
-}
-if (localStorage.getItem('17-pm')) {
-  $('.js-5-pm').val(`${localStorage.getItem('17-pm')}`);
-}
 
+
+
+  if (localStorage.getItem('9')) {
+    $('.js-9-am').val(`${localStorage.getItem('9')}`);
+  }
+  if (localStorage.getItem('10')) {
+    $('.js-10-am').val(`${localStorage.getItem('10')}`);
+  }
+  if (localStorage.getItem('11')) {
+    $('.js-11-am').val(`${localStorage.getItem('11')}`);
+  }
+  if (localStorage.getItem('12')) {
+    $('.js-12-am').val(`${localStorage.getItem('12')}`);
+  }
+  if (localStorage.getItem('13')) {
+    $('.js-13-pm').val(`${localStorage.getItem('13')}`);
+  }
+  if (localStorage.getItem('14')) {
+    $('.js-14-pm').val(`${localStorage.getItem('14')}`);
+  }
+  if (localStorage.getItem('15')) {
+    $('.js-15-pm').val(`${localStorage.getItem('15')}`);
+  }
+  if (localStorage.getItem('16')) {
+    $('.js-16-pm').val(`${localStorage.getItem('16')}`);
+  }
+  if (localStorage.getItem('17')) {
+    $('.js-17-pm').val(`${localStorage.getItem('17')}`);
+  }
 
 checkTime();
-setInterval(function(){checkTime()}, 1000);
+setInterval(function(){checkTime(), alertMessage()}, 1000);
 
   
 
